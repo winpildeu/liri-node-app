fdbbe303cec8c9a05639210df62533f65ff15f67
Winfield H <winterfreshness@gmail.com>
2018-12-05 17:57:06 -0500

    concert-this works.

--- liri.js
+++ liri.js
@@ -4,10 +4,11 @@ const result = require("dotenv").config();
 
 const fs = require('fs');
 const keys = require("./keys");
-console.table(keys);
+// console.table(keys);
 
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
+const moment = require('moment');
 const inquirer = require('inquirer');
 const axios = require('axios');
 
@@ -16,12 +17,21 @@ const axios = require('axios');
 function bandsInTown(artist) {
     let url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-    console.log(`Searching for ${artist} concerts...`);
+    console.log(`Searching for ${artist} concerts...\nURL: ${url}\n`);
     
     // perform a get request with the URL and artist info
     axios.get(url)
         .then(function (response) {
-            console.log(response);
+            // show the raw data response
+            // console.log(response);
+
+            // keep only the useful data
+            let data = response.data;
+
+            // display the name, location and date of each venue
+            data.forEach(element => {
+                console.log(`Name: ${element.venue.name} \nLocation: ${element.venue.city}, ${element.venue.country} \nDate: ${moment(response.datetime).format("MM/DD/YYYY")}\n\n`);
+            });
         })
         .catch(function (error) {
             console.log(error);
